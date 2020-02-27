9a71de18087ef1bbbf3189874db9ec3fd7df2235
Kwon770 <sckwon770@gmail.com>
2020-02-27 17:24:50 +0900

    1-1 userInput part one

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    setValue(value);
+  };
+  return { value, onChange };
+};
+
+// {...name} : unpack name all. name 안에 있는 것을 모두 풀어서 알아서 적용함
 const App = () => {
+  const name = useInput("Mr.");
   return (
     <div className="App">
       <h1>Hello</h1>
+      <input placeholder="name" {...name} />
     </div>
   );
 };
